de16d32329828f97398cec79646fbb275042d26d
dwddgsz <dwddgsz@gmail.com>
2020-12-26 14:05:34 +0100

    Add: routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import {Router,Switch,Route} from 'react-router';
+import history from './history'
+import Nav from './components/layout/Nav';
+import Home from './components/layout/Home';
+import Create from './components/layout/Create';
+import Edit from './components/layout/Edit';
+import NotFound from './components/layout/NotFound';
 
 const App = () => {
     return (
-        <div>
-            app
-        </div>
+    <>
+      <Router history={history}>
+        <Nav></Nav>
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/create" exact component={Create} />
+      <Route path="/edit" exact component={Edit} />
+      <Route component={NotFound} />
+      </Switch>
+      </Router>
+    </>
     )
 }
 
