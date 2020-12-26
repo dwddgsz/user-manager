47e8df2a745af9f45a712fd007bb13ae319d74ff
dwddgsz <dwddgsz@gmail.com>
2020-12-26 20:48:33 +0100

    Add: html structure, styles and functionality

--- src/components/layout/NotFound.js
+++ src/components/layout/NotFound.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import Subtitle from '../reusable/Subtitle';
+import styled from 'styled-components';
+import history from '../../history'
+
+
+
+const NotFoundWrapper = styled.section`
+button {
+    display:block;
+        margin:15px auto 0;
+        padding: 5px 20px;
+        border:2px solid var(--dark);
+        border-radius:4px;
+        color:var(--white);
+        background-color: var(--dark);
+        cursor:pointer;
+        transition: background-color .3s, border .3s, color .3s;
+        &:hover,&:focus {
+            background-color:var(--white);
+            border: 2px solid var(--dark);
+            color: var(--dark);
+        }
+}
+`
+
 
 const NotFound = () => {
+    const pushToHome = () => {
+        history.push('/')
+    }
     return (
-        <div>
-            notfound
-        </div>
+        <NotFoundWrapper>
+        <Subtitle>Page Not Found</Subtitle>
+        <button onClick={pushToHome}>Home</button>
+        </NotFoundWrapper>
     )
 }
 
